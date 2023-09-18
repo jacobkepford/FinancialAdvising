01e6ced72996246bf8d31a9bec5ff4c0c78d0645
Jacob Kepford <rtbredge@gmail.com>
2023-09-18 15:57:20 -0500

    Create investment built

--- main/main.go
+++ main/main.go
@@ -1,1 +1,21 @@
 package main
+
+type User struct {
+	userName       string
+	investmentList []Investment
+}
+
+func (u *User) CreateInvestment(investment Investment) {
+	if !investment.isEmployerMatch && investment.employerMatchPercent > 0 {
+		return
+	}
+
+	u.investmentList = append(u.investmentList, investment)
+}
+
+type Investment struct {
+	currentAmount        int
+	isEmployerMatch      bool
+	employerMatchPercent float32
+	investmentPercent    float32
+}
--- main/main_test.go
+++ main/main_test.go
@@ -1,1 +1,28 @@
 package main
+
+import "testing"
+
+func TestCreateInvestment(t *testing.T) {
+	t.Run("Test creating valid investment", func(t *testing.T) {
+		user := User{userName: "Jacob Kepford"}
+		investment := Investment{currentAmount: 100000, isEmployerMatch: true, employerMatchPercent: 7.5, investmentPercent: 11}
+
+		user.CreateInvestment(investment)
+
+		if len(user.investmentList) == 0 {
+			t.Error("Investment was not found for provided user")
+		}
+	})
+
+	t.Run("Test creating investment with invalid employer match", func(t *testing.T) {
+		user := User{userName: "Jacob Kepford"}
+		investment := Investment{currentAmount: 100000, isEmployerMatch: false, employerMatchPercent: 7.5, investmentPercent: 11}
+
+		user.CreateInvestment(investment)
+
+		if len(user.investmentList) > 0 {
+			t.Error("Employer match is false but employer match percent is greater than 0")
+		}
+	})
+
+}
