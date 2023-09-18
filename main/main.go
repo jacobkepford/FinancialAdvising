465e4acc571fefff5f4caf993a65b5954e86313e
Jacob Kepford <rtbredge@gmail.com>
2023-09-18 16:16:02 -0500

    validating ID being created for investment

--- main/main.go
+++ main/main.go
@@ -7,13 +7,16 @@ type User struct {
 
 func (u *User) CreateInvestment(investment Investment) {
 	if !investment.isEmployerMatch && investment.employerMatchPercent > 0 {
-		return
+		investment.employerMatchPercent = 0
 	}
+
+	investment.id = len(u.investmentList) + 1
 
 	u.investmentList = append(u.investmentList, investment)
 }
 
 type Investment struct {
+	id                   int
 	currentAmount        int
 	isEmployerMatch      bool
 	employerMatchPercent float32
--- main/main_test.go
+++ main/main_test.go
@@ -3,25 +3,44 @@ package main
 import "testing"
 
 func TestCreateInvestment(t *testing.T) {
-	t.Run("Test creating valid investment", func(t *testing.T) {
+	t.Run("Validate proper ID was created for investment", func(t *testing.T) {
 		user := User{userName: "Jacob Kepford"}
 		investment := Investment{currentAmount: 100000, isEmployerMatch: true, employerMatchPercent: 7.5, investmentPercent: 11}
 
 		user.CreateInvestment(investment)
 
-		if len(user.investmentList) == 0 {
-			t.Error("Investment was not found for provided user")
+		got := user.investmentList[0].id
+		want := 1
+
+		if got != want {
+			t.Errorf("Expected %d got %d", want, got)
 		}
 	})
 
-	t.Run("Test creating investment with invalid employer match", func(t *testing.T) {
+	t.Run("Validate employer match set when isEmployerMatch is true", func(t *testing.T) {
+		user := User{userName: "Jacob Kepford"}
+		investment := Investment{currentAmount: 100000, isEmployerMatch: true, employerMatchPercent: 7.5, investmentPercent: 11}
+
+		user.CreateInvestment(investment)
+
+		got := user.investmentList[0].employerMatchPercent
+		want := investment.employerMatchPercent
+
+		if got != want {
+			t.Errorf("Expected %f got %.2f", want, got)
+		}
+	})
+
+	t.Run("Test creating investment with no employer match but employer match percent", func(t *testing.T) {
 		user := User{userName: "Jacob Kepford"}
 		investment := Investment{currentAmount: 100000, isEmployerMatch: false, employerMatchPercent: 7.5, investmentPercent: 11}
 
 		user.CreateInvestment(investment)
 
-		if len(user.investmentList) > 0 {
-			t.Error("Employer match is false but employer match percent is greater than 0")
+		got := user.investmentList[0].employerMatchPercent
+
+		if got != 0 {
+			t.Errorf("Expected 0 got %.2f", got)
 		}
 	})
 
